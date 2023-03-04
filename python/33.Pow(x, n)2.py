c077d137de42f494e2f59f13ab406cc59d597dd9
Brandon <bbkx226@gmail.com>
2023-03-04 21:45:43 +0800

    Day 21

--- python/33.Pow(x, n)2.py
+++ python/33.Pow(x, n)2.py
@@ -8,7 +8,7 @@ class Solution:
             # 在对 N 进行二进制拆分的同时计算答案
             while N > 0:
                 if N % 2 == 1:
-                    # 如果 N 二进制表示的最低位为 1 (即奇数），那么需要计入贡献
+                    # 如果 N 二进制表示的最低位为 1，那么需要计入贡献
                     ans *= x_contribute
                 # 将贡献不断地平方
                 x_contribute *= x_contribute
