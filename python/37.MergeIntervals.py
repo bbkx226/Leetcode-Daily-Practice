a536e0572d553ae46ec65776332277150e4bdd43
Brandon <bbkx226@gmail.com>
2023-03-08 21:21:38 +0800

    Add more comments on the algorithm

--- python/37.MergeIntervals.py
+++ python/37.MergeIntervals.py
@@ -1,4 +1,14 @@
 # 排序
+
+# 我们用数组存储最终的答案
+# 首先，我们将列表中的区间按照左端点升序排序。然后我们将第一个区间加入 数组中，并按顺序依次考虑之后的每个区间：merged
+# 如果当前区间的左端点在数组 中最后一个区间的右端点之后，那么它们不会重合，我们可以直接将这个区间加入数组 的末尾；merged
+# 否则，它们重合，我们需要用当前区间的右端点更新数组 中最后一个区间的右端点，将其置为二者的较大值。merged
+# For Example,
+# merged = [[1, 3]], 要比较的list = [2, 6]
+# 由于2 < 3, b不能直接加， 需要合并
+# 比较两个list中哪个的第二值比较大，就等于哪个
+
 from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
