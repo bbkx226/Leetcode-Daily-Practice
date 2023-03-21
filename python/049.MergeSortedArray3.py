9db0a1ddd5d9e84d83f5b8016105497d0d7c068f
Brandon <bbkx226@gmail.com>
2023-03-21 13:47:56 +0800

    Day 30

---
+++ python/049.MergeSortedArray3.py
@@ -0,0 +1,28 @@
+# 逆向双指针
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1, p2 = m - 1, n - 1
+        tail = m + n - 1
+        while p1 >= 0 or p2 >= 0:
+            if p1 == -1:
+                nums1[tail] = nums2[p2]
+                p2 -= 1
+            elif p2 == -1:
+                nums1[tail] = nums1[p1]
+                p1 -= 1
+            elif nums1[p1] > nums2[p2]:
+                nums1[tail] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[tail] = nums2[p2]
+                p2 -= 1
+            tail -= 1
+        return nums1
+
+sol = Solution()
+print(sol.merge([1,2,3,0,0,0], 3, [2, 5, 6], 3))
