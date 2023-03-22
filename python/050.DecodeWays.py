98cc89379c014cd3f8c0a26cfac150799367fed2
Brandon <bbkx226@gmail.com>
2023-03-22 23:42:02 +0800

    Day 31

---
+++ python/050.DecodeWays.py
@@ -0,0 +1,17 @@
+# 动态规划
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        # a = f[i-2], b = f[i-1], c = f[i]
+        a, b, c = 0, 1, 0
+        for i in range(1, n + 1):
+            c = 0
+            if s[i - 1] != '0':
+                c += b
+            if i > 1 and s[i - 2] != '0' and int(s[i-2:i]) <= 26:
+                c += a
+            a, b = b, c
+        return c
+
+sol = Solution()
+print(sol.numDecodings("12"))
