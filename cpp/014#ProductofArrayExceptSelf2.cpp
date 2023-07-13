bdcd58c85dd463d5accf49213c59517bd3da1aa9
Brandon Ban Kai Xian <bbkx226@gmail.com>
2023-07-13 20:54:42 +0800

    Progress: 14/150 - Method 2 Done

---
+++ cpp/014#ProductofArrayExceptSelf2.cpp
@@ -0,0 +1,42 @@
+// 空间复杂度 O(1) 的方法
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+# include <iostream>
+# include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int length = nums.size();
+        vector<int> answer(length);
+
+        // answer[i] 表示索引 i 左侧所有元素的乘积
+        // 因为索引为 '0' 的元素左侧没有元素， 所以 answer[0] = 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+
+        // R 为右侧所有元素的乘积
+        // 刚开始右边没有元素，所以 R = 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            // 对于索引 i，左边的乘积为 answer[i]，右边的乘积为 R
+            answer[i] = answer[i] * R;
+            // R 需要包含右边所有的乘积，所以计算下一个结果时需要将当前值乘到 R 上
+            R *= nums[i];
+        }
+        return answer;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> answer = s.productExceptSelf(nums);
+    for (int i = 0; i < answer.size(); i++) cout << answer[i] << " ";
+    cout << endl;
+    return 0;
+}
