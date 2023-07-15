0a9de7682ab9d2e644c0a3b4d2469c131b0bd389
Brandon Ban Kai Xian <bbkx226@gmail.com>
2023-07-15 22:12:05 +0800

    Progress: 25/150 - Done

---
+++ cpp/025#TextJustification.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string fillWords(vector<string>& words, int bg, int ed, int maxWidth, bool lastLine = false)
+    {
+        int wordCount = ed - bg + 1;
+        int spaceCount = maxWidth + 1 - wordCount;  // 除去每个单词尾部空格， + 1 是最后一个单词的尾部空格的特殊处理
+        for (int i = bg; i <= ed; i++)
+        {
+            spaceCount -= words[i].size();  // 除去所有单词的长度
+        }
+
+        int spaceSuffix = 1;    // 词尾空格
+        int spaceAvg = (wordCount == 1) ? 1 : spaceCount / (wordCount - 1);     // 额外空格的平均值
+        int spaceExtra = (wordCount == 1) ? 0 : spaceCount % (wordCount - 1);   // 额外空格的余数
+
+        string ans;
+        for (int i = bg; i < ed; i++)
+        {
+            ans += words[i];    // 填入单词
+            if (lastLine)   // 特殊处理最后一行
+            {
+                fill_n(back_inserter(ans), 1, ' ');
+                continue;
+            }
+            fill_n(back_inserter(ans), spaceSuffix + spaceAvg + ((i - bg) < spaceExtra), ' ');  // 根据计算结果补上空格
+        }
+        ans += words[ed];   // 填入最后一个单词
+        fill_n(back_inserter(ans), maxWidth - ans.size(), ' '); // 补上这一行最后的空格
+        return ans;
+    }
+
+    vector<string> fullJustify(vector<string>& words, int maxWidth) 
+    {
+        vector<string> ans;
+        int cnt = 0; // 当前行的长度
+        int bg = 0; // 当前行的第一个单词的下标
+        for (int i = 0; i < words.size(); i++)
+        {
+            cnt += words[i].size() + 1; // 加上当前单词的长度和一个空格
+
+            if (i + 1 == words.size() || cnt + words[i + 1].size() > maxWidth)
+            {   // 如果是最后一个单词，或者加上下一个词就超过长度了，即可凑成一行
+                ans.push_back(fillWords(words, bg, i, maxWidth, i + 1 == words.size()));
+                bg = i + 1;
+                cnt = 0;
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    vector<string> words = { "This", "is", "an", "example", "of", "text", "justification." };
+    int maxWidth = 16;
+    vector<string> ans = solution.fullJustify(words, maxWidth);
+    for (auto& s : ans)
+    {
+        cout << s << endl;
+    }
+    return 0;
+}
